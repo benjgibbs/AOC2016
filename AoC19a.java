39af0d7c7bcf1ad8b30a0f6bffab9cbc9cab87a9
Ben Gibbs <benjgibbs@gmail.com>
2016-12-19 18:02:30 +0000

    Use josephus closed forms for a part

--- AoC19a.java
+++ AoC19a.java
@@ -6,29 +6,51 @@ class AoC19a {
     run(5);
     run(3014603);
   }
+  
+  static class Elf {
+    final int id;
+    Elf n;
+    Elf(int id) { this.id = id; } 
+  }
 
-  static void run(int ne) { 
-    int[] presents = new int[ne];
-    
-    for (int i = 0; i < ne; i++){
-      presents[i] = 1;
+  static void run(int ne) {
+    bruteForce(ne);
+    josephus(ne);
+    josephus2(ne);
+  }
+
+  static void josephus(int ne) {
+    int i = 1;
+    while (i < ne) {
+      i *= 2;
     }
+    i /= 2;
+    int l = ne - i;
+    System.out.println((2 * l + 1) + " by Josepheus");
+  }
+  
+  static void josephus2(int ne) {
+    String s = Integer.toString(ne, 2);
+    int i = s.indexOf("1");
+    s = s.substring(i+1) + "1";
+    System.out.println(Integer.parseInt(s, 2) + " by Josepheus bit trick");
+  }
+
+  static void bruteForce(int ne) {
+    Elf h = new Elf(1);
+    Elf n = h; 
+    for (int i = 2; i <= ne; i++) {
+      n.n = new Elf(i);
+      n = n.n;
+    }
+    n.n = h;
     
-    int i = 0;
-    while (true) {
-      if (presents[i] != 0) {
-        int j = (i + 1) % ne;
-        while (presents[j] == 0) {
-          j = (j+1) % ne;
-        }
-        if (i == j) {
-          System.out.println((i+1) + " has all the presents");
-          break;
-        }
-        presents[i] += presents[j];
-        presents[j] = 0;
-      }
-      i = (i + 1) % ne;
-    }  
+    n = h; 
+    while (n != n.n) {
+      n.n = n.n.n;
+      n = n.n;
+    }
+
+    System.out.println(n.id + " has all the presents");
   }
 }
